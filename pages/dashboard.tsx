c14c3221fb39761c0de45c0b5794fcc292720114
Wahab Shaikh <wahab.shaikh@somaiya.edu>
2021-12-11 20:24:06 +0530

    Protect the routes

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,15 +1,34 @@
-import type { NextPage } from "next";
+import { User } from "@supabase/supabase-js";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
+import supabase from "../lib/supabase";
 
-const Dashboard: NextPage = () => {
+interface IDashboard {
+  user: User;
+}
+
+const Dashboard: NextPage<IDashboard> = ({ user }) => {
   return (
     <>
       <Head>
-        <title>Dashboard | Supa Sceret Santa</title>
+        <title>Dashboard | Supa Secret Santa</title>
       </Head>
       <h1 className="text-3xl font-bold underline">Dashboard</h1>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const { user } = await supabase.auth.api.getUserByCookie(req);
+
+  if (!user) {
+    return {
+      props: {},
+      redirect: { destination: "/login", permanent: "false" },
+    };
+  }
+
+  return { props: { user } };
 };
 
 export default Dashboard;
--- pages/room/[id].tsx
+++ pages/room/[id].tsx
@@ -1,19 +1,38 @@
-import type { NextPage } from "next";
+import { User } from "@supabase/supabase-js";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import supabase from "../../lib/supabase";
 
-const Room: NextPage = () => {
+interface IRoom {
+  user: User;
+}
+
+const Room: NextPage<IRoom> = ({ user }) => {
   const router = useRouter();
   const { id } = router.query;
 
   return (
     <>
       <Head>
-        <title>Room {id} | Supa Sceret Santa</title>
+        <title>Room {id} | Supa Secret Santa</title>
       </Head>
       <h1 className="text-3xl font-bold underline">Room {id}</h1>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const { user } = await supabase.auth.api.getUserByCookie(req);
+
+  if (!user) {
+    return {
+      props: {},
+      redirect: { destination: "/login", permanent: "false" },
+    };
+  }
+
+  return { props: { user } };
 };
 
 export default Room;
